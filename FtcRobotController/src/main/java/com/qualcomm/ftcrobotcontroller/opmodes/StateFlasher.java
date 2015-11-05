20dbe8010e1d22024448356d5fa5fc0e67d60438
Mark Hancock <engineer@hancockhome.com>
2015-11-04 21:01:30 -0800

    Make DelayState public

--- FtcRobotController/src/main/java/com/qualcomm/ftcrobotcontroller/opmodes/StateFlasher.java
+++ FtcRobotController/src/main/java/com/qualcomm/ftcrobotcontroller/opmodes/StateFlasher.java
@@ -71,45 +71,6 @@ class FlashState extends OpState{
 }
 
 /**
- * State that delays for a fixed time then moves to the specified state
- */
-class DelayState extends OpState {
-
-    private StateFlasher opMode;
-    private int DelayCounter = 0;
-    private int DelayCount;
-    private String NextStateName;
-
-    /**
-     * Constructor
-     *
-     * @param name       State Name
-     * @param opmode     OpMode
-     * @param count      Number of times to flash
-     * @param next_state Next State Name
-     */
-    DelayState(String name, StateFlasher opmode, int count, String next_state) {
-        super(name);
-        opMode = opmode;
-        DelayCount = count;
-        NextStateName = next_state;
-    }
-
-    @Override
-    public void OnEntry() {
-        DelayCounter = 0; //reset the delay count
-    }
-
-    @Override
-    public void Do() {
-        DelayCounter++;
-        opMode.telemetry.addData(Name, "Delaying");
-        opMode.telemetry.addData(Name, "Count = " + String.format("%d", DelayCounter));
-        if(DelayCounter>=DelayCount) SetCurrentState(NextStateName);
-    }
-}
-
-/**
  * Simple OpMode to demonstrate control of the Camera Light
  */
 public class StateFlasher extends OpMode {
