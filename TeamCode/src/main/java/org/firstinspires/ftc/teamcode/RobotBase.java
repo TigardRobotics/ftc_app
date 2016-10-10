33dd48c2b65b9969adc72dcd5576030ec1f0e886
FTC3965 <tigardrobotics@gmail.com>
2016-10-09 18:52:25 -0700

    Added a RobotBase class for 2016 and a BasicTeleop class that inherits from it.

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/RobotBase.java
@@ -0,0 +1,35 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.util.ElapsedTime;
+
+/**
+ * Created by Derek Williams of team 3965 on 10/9/2016.
+ */
+
+public abstract class RobotBase extends OpMode {
+    private DcMotor leftMotor = null;
+    private DcMotor rightMotor = null;
+
+    @Override
+    public void init() {
+        telemetry.addData("Status", "Initialized");
+        leftMotor = hardwareMap.dcMotor.get("motor_l");
+        rightMotor = hardwareMap.dcMotor.get("motor_r");
+    }
+
+    public void setDrivePower(double power){
+        leftMotor.setPower(power);
+        rightMotor.setPower(power);
+    }
+
+    public void setLeftDrivePower(double power){
+        leftMotor.setPower(power);
+    }
+
+    public void setRightDrivePower(double power){
+        rightMotor.setPower(power);
+    }
+}
+
