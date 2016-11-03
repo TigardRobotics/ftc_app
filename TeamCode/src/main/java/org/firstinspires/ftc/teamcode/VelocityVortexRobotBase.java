06cf71d5ead548df68b178ce39a157f1e7f95475
FTC3965 <tigardrobotics@gmail.com>
2016-11-02 20:03:35 -0700

    added broken handling of button pusher servos

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/VelocityVortexRobotBase.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/VelocityVortexRobotBase.java
@@ -1,5 +1,7 @@
 package org.firstinspires.ftc.teamcode;
 
+import com.qualcomm.robotcore.hardware.Servo;
+
 /**
  * Created by Robotics on 10/11/2016.
  */
@@ -9,6 +11,16 @@ public abstract class VelocityVortexRobotBase extends RobotBase {
     protected static final double fullMotorRotation = 7660;
 
     // All hardware custom to velocity vortex defined here
+    protected Servo rightButtonPusher;
+    protected Servo leftButtonPusher;
+
+    @Override
+    public void init() {
+        super.init();
+        rightButtonPusher = hardwareMap.servo.get("right_button_pusher");
+        leftButtonPusher = hardwareMap.servo.get("left_button_pusher");
+        telemetry.addLine("Velocity Vortex Base Initialized");
+    }
 
     @Override
     public double getDriveMotorDiameter () {
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/VelocityVortexTeleOp.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/VelocityVortexTeleOp.java
@@ -1,15 +1,44 @@
 package org.firstinspires.ftc.teamcode;
 
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.util.Range;
+
 /**
  * Created by Derek Williams on 10/11/2016.
  */
 
+@TeleOp(name="VV Teleop", group="3965")
 public class VelocityVortexTeleOp extends VelocityVortexRobotBase {
+
     @Override
     public void start(){}
 
     @Override
-    public void loop(){}
+    public void loop(){
+        setLeftDrivePower(-gamepad1.left_stick_y);
+        setRightDrivePower(-gamepad1.right_stick_y);
+
+        if (gamepad1.a) {
+            rightButtonPusher.setPosition(100/128);
+            telemetry.addLine("R OUT");
+        }
+        else if (gamepad1.b) {
+            rightButtonPusher.setPosition(50/128);
+            telemetry.addLine("R IN");
+        }
+
+        if(gamepad1.x) {
+            leftButtonPusher.setPosition(100/128);
+            telemetry.addLine("L OUT");
+        }
+        else if (gamepad1.y) {
+            leftButtonPusher.setPosition(50/128);
+            telemetry.addLine("L IN");
+        }
+
+        //telemetry.addData("Right pusher pos", rightButtonPusher.getPosition());
+        //telemetry.addData("Left pusher pos", leftButtonPusher.getPosition());
+    }
 
     @Override
     public void stop(){}
