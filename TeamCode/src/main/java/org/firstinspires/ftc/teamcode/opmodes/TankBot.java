f3538c3d49461cf33f6e0b09277f3c55b8cecaf1
Mark Hancock <engineer@hancockhome.com>
2017-10-01 19:39:21 -0700

    Completed implementation of Controllers concept for the LED and TankDrive Controllers Added TankBot for hardware that used to be in RobotBase MoveState now looks up drive interface in the Controllers RobotBase is now (mostly) hardware free

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/TankBot.java
@@ -0,0 +1,35 @@
+package org.firstinspires.ftc.teamcode.opmodes;
+
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DeviceInterfaceModule;
+
+import org.firstinspires.ftc.teamcode.controllers.HardwareController;
+import org.firstinspires.ftc.teamcode.controllers.IDrive;
+import org.firstinspires.ftc.teamcode.controllers.LedController;
+import org.firstinspires.ftc.teamcode.controllers.TankDrive;
+
+/**
+ * This is a basic Tank Drive Robot
+ */
+
+public abstract class TankBot extends RobotBase {
+
+    private DcMotor leftDriveMotor;
+    private DcMotor rightDriveMotor;
+    public IDrive Drive;
+
+    public void init() {
+        super.init();
+        leftDriveMotor = hardwareMap.dcMotor.get("motor_l");
+        rightDriveMotor = hardwareMap.dcMotor.get("motor_r");
+        Controllers.add(new TankDrive(rightDriveMotor, leftDriveMotor));
+
+        Drive = (IDrive)(HardwareController.Find(Controllers,IDrive.class));
+    }
+
+    @Override
+    public void stop() {
+        super.stop();
+        Drive.stopDriveMotors();
+    }
+}
