4a51d95ee0d90f068f2ad9fc817275996a85c86c
Mark Hancock <engineer@hancockhome.com>
2017-10-09 21:08:28 -0700

    Account for Loop time in accel

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/controllers/AccelDrive.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/controllers/AccelDrive.java
@@ -2,6 +2,7 @@ package org.firstinspires.ftc.teamcode.controllers;
 
 
 import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.util.ElapsedTime;
 import com.qualcomm.robotcore.util.RobotLog;
 
 /**
@@ -12,11 +13,15 @@ public class AccelDrive extends TankDrive {
 
     private double targetLeftPower = 0;
     private double targetRightPower = 0;
+    private ElapsedTime loopTimer;
 
-    public double Accel = 1;    //Max accel
+    public double accel;    //Motor accel/time  1=0 to full power in 1 sec
 
-    public AccelDrive(DcMotor right, DcMotor left) {
+    public AccelDrive(DcMotor right, DcMotor left, double accel) {
         super( right, left);
+        targetLeftPower = 0;
+        targetRightPower = 0;
+        this.accel = accel;
     }
 
     public void setLeftDrivePower(double power) {
@@ -27,31 +32,37 @@ public class AccelDrive extends TankDrive {
         targetRightPower = power;
     }
 
+    public void start() {
+        loopTimer.reset();
+    }
+
     public void loop() {
+        double loopTime=loopTimer.seconds();
+        double power_change=accel*loopTime;    //power change for this loop
         //Adjust Left power based on the accel
-        if(Math.abs(targetLeftPower-leftDrivePower)<=Accel) {
+        if(Math.abs(targetLeftPower-leftDrivePower)<=power_change) {
             super.setLeftDrivePower(targetLeftPower);
         }
         else if (leftDrivePower < targetLeftPower)
         {
-            super.setLeftDrivePower(leftDrivePower + Accel);
+            super.setLeftDrivePower(leftDrivePower + power_change);
         }
         else
         {
-            super.setLeftDrivePower(leftDrivePower - Accel);
+            super.setLeftDrivePower(leftDrivePower - power_change);
         }
 
         //Adjust Right power based on the accel
-        if(Math.abs(targetRightPower-rightDrivePower)<=Accel) {
+        if(Math.abs(targetRightPower-rightDrivePower)<=power_change) {
             super.setRightDrivePower(targetRightPower);
         }
         else if (rightDrivePower < targetRightPower)
         {
-            super.setRightDrivePower(rightDrivePower + Accel);
+            super.setRightDrivePower(rightDrivePower + power_change);
         }
         else
         {
-            super.setRightDrivePower(rightDrivePower - Accel);
+            super.setRightDrivePower(rightDrivePower - power_change);
         }
         super.loop();
     }
