91747b5015e40e59f410d03e44065afe365c198e
Mark Hancock <engineer@hancockhome.com>
2017-11-08 19:05:03 -0800

    <DEREK> Added Controller for jewel knocker arm and abstract direction controller

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/controllers/KnockerController.java
@@ -0,0 +1,26 @@
+package org.firstinspires.ftc.teamcode.controllers;
+
+import com.qualcomm.robotcore.hardware.Servo;
+
+/**
+ * Created by Derek on 11/8/2017.
+ */
+
+public class KnockerController extends HardwareController {
+    private static final double DOWN = 0.5;
+    private static final double UP = 0.5;
+
+    private Servo servo;
+
+    public KnockerController(Servo servo) {
+        this.servo = servo;
+    }
+
+    public void extend() {
+        servo.setPosition(DOWN);
+    }
+
+    public void retract() {
+        servo.setPosition(UP);
+    }
+}
