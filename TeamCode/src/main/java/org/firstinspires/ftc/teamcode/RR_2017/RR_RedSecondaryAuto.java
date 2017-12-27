a671d35750e67e55f943773975da336788eff427
FTC3965 <tigardrobotics@gmail.com>
2017-12-27 14:20:39 -0800

    Added key column detection and glyph placement to red secondary auto

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/RR_2017/RR_RedSecondaryAuto.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/RR_2017/RR_RedSecondaryAuto.java
@@ -2,6 +2,7 @@ package org.firstinspires.ftc.teamcode.RR_2017;
 
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 
+import org.firstinspires.ftc.robotcore.external.navigation.RelicRecoveryVuMark;
 import org.firstinspires.ftc.teamcode.Color;
 import org.firstinspires.ftc.teamcode.controllers.SwerveDrive;
 import org.firstinspires.ftc.teamcode.controllers.VuMarkController;
@@ -9,12 +10,14 @@ import org.firstinspires.ftc.teamcode.opmodes.SwerveBase;
 import org.firstinspires.ftc.teamcode.statemachines.ColorTrans;
 import org.firstinspires.ftc.teamcode.statemachines.CrabState;
 import org.firstinspires.ftc.teamcode.statemachines.DriveState;
+import org.firstinspires.ftc.teamcode.statemachines.GlobalTimeTrans;
 import org.firstinspires.ftc.teamcode.statemachines.KnockState;
 import org.firstinspires.ftc.teamcode.statemachines.PickUpBlockState;
 import org.firstinspires.ftc.teamcode.statemachines.ProgressTrans;
 import org.firstinspires.ftc.teamcode.statemachines.SpinState;
 import org.firstinspires.ftc.teamcode.statemachines.StateMachine;
 import org.firstinspires.ftc.teamcode.statemachines.TimeTrans;
+import org.firstinspires.ftc.teamcode.statemachines.VuMarkTrans;
 import org.firstinspires.ftc.teamcode.statemachines.WaitState;
 
 /**
@@ -60,21 +63,39 @@ public class RR_RedSecondaryAuto extends RR_AutoBase {
                 new KnockState("unknock from left", true, new TimeTrans("spin right back", 0.5)),
                 new SpinState("spin right back", 0.2, new ProgressTrans("to crab", 20.0)),
 
-
-
                 //Drive to cryptobox
                 new DriveState("to crab", 0.0, new TimeTrans("forward", 1.5)),
                 new DriveState("forward", 0.4,
-                        new ProgressTrans("to spin2", 80.0),
+                        new ProgressTrans("to crab right", 80.0),
                         new TimeTrans("end", 5.0)), //in case stall
-                new SpinState("to spin2", 0.0, new TimeTrans("spin", 1.5)),
-                new SpinState("spin", -0.3, new ProgressTrans("to push", 45.0)),
-                new DriveState("to push", 0.0, new TimeTrans("drop block", 1.5)),
+
+                new CrabState("to crab right", 90.0, 0.0, new TimeTrans("crab to wall", 1.5)),
+                new CrabState("crab to wall", 90.0, 0.2, new TimeTrans("key column", 5.0)),
+                new WaitState("key column",
+                        new VuMarkTrans("to right column", RelicRecoveryVuMark.RIGHT),
+                        new VuMarkTrans("to left column", RelicRecoveryVuMark.LEFT),
+                        new VuMarkTrans("to center column", RelicRecoveryVuMark.CENTER),
+                        new GlobalTimeTrans("to center column", 10.0) // Default to center column
+                ),
+
+                new CrabState("to right column", 90.0, -0.4,
+                        new ProgressTrans("to forward", 70.0),
+                        new TimeTrans("end", 5.0) //in case stall
+                ),
+                new CrabState("to center column", 90.0, -0.4,
+                        new ProgressTrans("to forward", 92.0),
+                        new TimeTrans("end", 5.0) //in case stall
+                ),
+                new CrabState("to left column", 90.0, -0.4,
+                        new ProgressTrans("to forward", 120.0),
+                        new TimeTrans("end", 5.0) //in case stall
+                ),
+
+                new CrabState("to forward", 0.0, 0.0, new TimeTrans("drop block", 0.5)),
                 new PickUpBlockState("drop block", 1.0, true, new TimeTrans("ram block", 1.0)),
                 new DriveState("ram block", 0.5, new TimeTrans("backup", 2.0)),
                 new DriveState("backup", -0.5, new TimeTrans("end", 0.3)),
                 new WaitState("end")
-
         );
     }
 }
